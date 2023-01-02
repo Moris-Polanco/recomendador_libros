aa825b0ff6d042be4745534e51e0c26cdd940016
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-02 08:33:22 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,35 @@
+import openai
+import streamlit as st
+
+openai.api_key = 
+
+def obtener_recomendaciones(genero, edad, sexo, autores_favoritos):
+  model_engine = "text-davinci-003"
+  prompt = (f"Recomendaciones de libros en español para un {sexo} de {edad} años que disfruta de {genero} y cuya autores favoritos son {autores_favoritos}")
+
+  completions = openai.Completion.create(
+      engine=model_engine,
+      prompt=prompt,
+      max_tokens=1024,
+      n=1,
+      stop=None,
+      temperature=0.5,
+  )
+
+  respuesta = completions.choices[0].text
+  recomendaciones = respuesta.split("\n")[2:]
+  return recomendaciones
+
+st.title("Recomendador de libros personalizado")
+st.markdown("Este es un recomendador de libros personalizado que utiliza GPT-3 para proporcionar recomendaciones en español basadas en tus intereses y preferencias.")
+
+genero = st.text_input("¿Qué tipo de libros en español te gustaría leer?", "ciencia ficción")
+edad = st.number_input("¿Cuál es tu edad?", min_value=0, max_value=100)
+sexo = st.radio("¿Cuál es tu género?", ("Hombre", "Mujer", "Otro"))
+autores_favoritos = st.text_input("¿Cuáles son tus autores favoritos?", "Isaac Asimov, Arthur C. Clarke")
+
+recomendaciones = obtener_recomendaciones(genero, edad, sexo, autores_favoritos)
+
+st.header("Recomendaciones:")
+for recomendacion in recomendaciones:
+  st.markdown(f"- {recomendacion}")
